9017ba22faa083df02bf6893219b0a6cb750e7ee
Taher Abbasi <abbasi.taher@GMail.com>
2020-11-02 23:06:32 +0330

    footer_adder and exceptions are developed

---
+++ redlightmanager/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# This work is licensed under the MIT License.
+# To view a copy of this license, visit https://www.gnu.org/licenses/
+
+# Written by Taher Abbasi
+# Email: abbasi.taher@gmail.com
+
+class WrongSide(Exception):
+    pass
